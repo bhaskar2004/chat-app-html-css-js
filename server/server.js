8276290250d1592a17c836ecd46944bcd80083cb
bhaskar2004 <bhaskarbhaskar4794@gmail.com>
2025-01-16 10:19:31 +0530

    changed the server url

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
 });
 
 const path = require('path');
-const PORT = process.env.PORT || 3000;
+const PORT =  3000;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname)));
